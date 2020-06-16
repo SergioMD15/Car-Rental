c7adffef7a9b097cf27cd3e41b1b7eaa38c26f17
SergioMD15 <sergiomosquera96@gmail.com>
2020-06-16 15:58:12 +0200

    Level 2

--- src/controllers/RentalController.py
+++ src/controllers/RentalController.py
@@ -2,10 +2,19 @@ from model.Car import Car
 from model.Rental import Rental
 from controllers.IOController import IOController
 
+
 class RentalController:
 
     def __init__(self):
         self.io = IOController()
+        self.init_discounts()
+
+    def init_discounts(self):
+        self.discounts = {
+            10: 0.5,
+            4: 0.7,
+            1: 0.9
+        }
 
     def read_input(self, filename: str):
         result = self.io.read_file(filename)
@@ -18,18 +27,27 @@ class RentalController:
                 self.compute_daily_price(car, rental.get_days_rented()))
 
     def compute_daily_price(self, car, rented_days) -> int:
-        """Computes the total daily price for a given rental."""
-        return car.price_per_day * rented_days
+        """Computes the total discounted daily price for a given rental.
+
+        If the rental fulfills some temporal conditions, it receives a discount
+        for some period of time (these periods and its associated discounts are
+        stored in a class variable).
+        """
+        total_price = car.price_per_day
+        for days, discount in self.discounts.items():
+            if rented_days > days:
+                discount_days = rented_days - days
+                total_price += car.price_per_day * discount * (discount_days)
+                rented_days -= discount_days
+        return int(total_price)
 
     def compute_km_price(self, car, rental) -> int:
         return car.price_per_km * rental.distance
 
     def output_rental_recipe(self, filename: str):
         """
-        
         Creates the structure of the recipe for the IOController.
         Each rental is identified with its id and its price.
-        
         """
         self.io.output_rental_recipe(
             {'rentals':
